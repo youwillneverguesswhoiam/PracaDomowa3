154fcabc2d510f56a69ae10849e9d10d9f5cb42b
radziq1302 <radziq1302@gmail.com>
2017-11-29 16:07:38 +0100

    20171129
    takie tam zmianki początkowe

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,24 @@
 package sample;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.layout.Pane;
+
+import java.io.IOException;
+
 public class Controller {
+    public Pane plansza;
+    public void plansza1(ActionEvent actionEvent) {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("login.fxml"));
+        try {
+            Parent pane = loader.load();
+            Login cntr = loader.getController();
+            cntr.setParentController(this);
+            plansza.getChildren().clear();
+            plansza.getChildren().add(pane);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
---
+++ src/sample/Login.java
@@ -0,0 +1,8 @@
+package sample;
+
+public class Login {
+    private Controller parentController;
+    public void setParentController(Controller parentController) {
+        this.parentController = parentController;
+    }
+}
